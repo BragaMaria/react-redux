e6ae00b05cc436b5a8a0aaebeaf39c26af54a8ca
BragaMaria <brag4.m@yandex.com>
2024-10-22 12:42:30 +0500

    withRouter, props.match.params

--- src/components/Profile/ProfileContainer.js
+++ src/components/Profile/ProfileContainer.js
@@ -3,13 +3,21 @@ import {Content} from "./Content";
 import axios from "axios";
 import {connect} from "react-redux";
 import {setUserProfile} from "../../redux/profile-reducer";
+import {useLocation, useParams} from 'react-router-dom';
 
+export function withRouter(ProfileContainer){
+  return(props)=>{
+    const match  = {params: useParams()};
+    const location = {location:useLocation()}
+    return <ProfileContainer {...props}  match = {match} location={location}/>
+  }
+}
 
 class ProfileContainer extends React.Component{
-
   componentDidMount = () =>{
+    let userId = this.props.match.params.userId
     axios
-      .get('https://social-network.samuraijs.com/api/1.0/profile/5')
+      .get(`https://social-network.samuraijs.com/api/1.0/profile/${userId}`)
       .then((data)=>{
         this.props.setUserProfile(data.data)
       })
@@ -28,5 +36,5 @@ let mstp = (state)=>({
 
 export default connect(mstp,{
   setUserProfile,
-})(ProfileContainer)
+})(withRouter(ProfileContainer))
 
